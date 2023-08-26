7d1034f7220b658b183e6e0b34ebbe33296b68cb
Nik <gaponov.nik.07@gmail.com>
2023-08-26 23:33:01 +0600

    MARWIN ADVENTURE
    Work was done on the visual, UI, main menu and other things were added.

--- main.py
+++ main.py
@@ -1,135 +1,330 @@
-# -*- coding: utf-8 -*-
-"""
-	CopyLeft 2021 Michael Rouves
-
-	This file is part of Pygame-DoodleJump.
-	Pygame-DoodleJump is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	Pygame-DoodleJump is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with Pygame-DoodleJump. If not, see <https://www.gnu.org/licenses/>.
-"""
-
-
+from MARWIN_ADVENTURE import *
+from button import Button
 import pygame, sys
-
 from singleton import Singleton
 from camera import Camera
 from player import Player
 from level import Level
 import settings as config
+pygame.init()
 
+# Window
+window = pygame.display.set_mode(config.DISPLAY,config.FLAGS)
+clock = pygame.time.Clock()
 
+# Function for displaying text
+def display_text(text, font_size, color, font_path, pos):
+    font = pygame.font.Font(font_path, font_size)
+    text_render = font.render(text, True, color) 
+    text_rect = text_render.get_rect(center=pos)
+    window.blit(text_render, text_rect) 
 
 class Game(Singleton):
-	"""
-	A class to represent the game.
+	# Class for managing the game
+	def __init__(self):
+		# Initialization
+		self.score = 0 # Record
+		self.paused = False # Pause
+		self.max_darkness_reached = False # Has maximum darkness been reached
+		self.frame_reverse_animation_end = True # End of reverse frame animation
+		self.frame_animation_end = False # End of frame animation
+		self.game_over_animation_end = False # End of game over image animation
+		# End of button animation
+		self.mainmenu_button_animation_end = False 
+		self.restart_button_animation_end = False
 
-	used to manage game updates, draw calls and user input events.
-	Can be access via Singleton: Game.instance .
-	(Check Singleton design pattern for more info)
-	"""
+		# Game over
+		self.game_over_image = pygame.image.load(config.game_over_image).convert()
+		self.gameover_rect_initial_y = -config.HALF_YWIN  # Initial position of game over vertically
+		self.gameover_rect_y = self.gameover_rect_initial_y  # Current offset of game over vertically
+		self.gameover_rect = self.game_over_image.get_rect(center=(config.HALF_XWIN,config.HALF_YWIN//3)) # Get image size
+		self.game_over_sound_played = False
 
-	# constructor called on new instance: Game()
-	def __init__(self) -> None:
+		# Frame images for pause, score
+		self.frame_image_pause = pygame.image.load(config.frame_image_pause).convert()
+		self.frame_image_score = pygame.image.load(config.frame_image_score).convert()
+		self.frame_rect_initial_y = -20  # Initial position of frame vertically
+		self.frame_rect_y = self.frame_rect_initial_y  # Current offset of frame vertically
 		
-		# ============= Initialisation =============
-		self.__alive = True
-		# Window / Render
-		self.window = pygame.display.set_mode(config.DISPLAY,config.FLAGS)
-		self.clock = pygame.time.Clock()
+		# Buttons: mainmenu, restart, resume
+		self.mainmenu_button_rect_initial_x = 0  # Initial position of mainmenu horizontally
+		self.restart_button_rect_initial_x = config.XWIN  # Initial position of restart horizontally
+		self.mainmenu_button_image = pygame.image.load(config.mainmenu_button_non_active)
+		self.restart_button_image = pygame.image.load(config.restart_button_non_active)
+		self.resume_button_image = pygame.image.load(config.resume_button_non_active)
 
-		# Instances
-		self.camera = Camera()
-		self.lvl = Level()
-		self.player = Player(
-			config.HALF_XWIN - config.PLAYER_SIZE[0]/2,# X POS
-			config.HALF_YWIN + config.HALF_YWIN/2,#      Y POS
-			*config.PLAYER_SIZE,# SIZE
-			config.PLAYER_COLOR#  COLOR
-		)
+		# Button sounds
+		self.mainmenu_button_sound = False
+		self.restart_button_sound = False
+		self.resume_button_sound = False
 
-		# User Interface
-		self.score = 0
-		self.score_txt = config.SMALL_FONT.render("0 m",1,config.GRAY)
-		self.score_pos = pygame.math.Vector2(10,10)
+		# Background
+		self.background_image = pygame.image.load(config.background_image).convert()
+		self.background_image = pygame.transform.scale(self.background_image, (config.XWIN, config.YWIN))
+		self.overlay_color = config.BLACK  # Dimming color
+		self.overlay_alpha = 200  # Dimming alpha channel
 
-		self.gameover_txt = config.LARGE_FONT.render("Game Over",1,config.GRAY)
-		self.gameover_rect = self.gameover_txt.get_rect(
-			center=(config.HALF_XWIN,config.HALF_YWIN))
+		######################Class instances######################
+		# Buttons
+		#For game over
+		self.mainmenu_button = Button(self.mainmenu_button_image, (self.mainmenu_button_rect_initial_x, config.YWIN // 1.2), action='')
+		self.restart_button = Button(self.restart_button_image, (self.restart_button_rect_initial_x, config.YWIN // 1.2), action='')
+		# For pause
+		self.mainmenu_button_pause = Button(self.mainmenu_button_image, (config.XWIN // 4, config.YWIN // 2), action='')
+		self.restart_button_pause = Button(self.restart_button_image, (config.XWIN // 2, config.YWIN // 2), action='')
+		self.resume_button = Button(self.resume_button_image, (config.XWIN // 1.34, config.YWIN // 2), action='')
+		# Objects
+		self.camera = Camera() # Camera
+		self.lvl = Level() # Level
+		self.player = Player( # Player
+			config.HALF_XWIN - config.PLAYER_SIZE[0]/2, # X position
+			config.HALF_YWIN + config.HALF_YWIN/2, # Y position
+			*config.PLAYER_SIZE,) # Size
 	
-	
-	def close(self):
-		self.__alive = False
+	def resume(self): # Resume pause
+		self.paused = False
+		
+	def reset(self): # Reset the game
+		if self.game_over_animation_end or self.paused:
+			pygame.mixer.music.play(-1)
+			self.paused = False
+			self.camera.reset()
+			self.lvl.reset()
+			self.player.reset()
 
+	def launch_main_menu(self): # Launch main menu and exit current loop
+		import MARWIN_ADVENTURE
+		MARWIN_ADVENTURE.Run()
+		pygame.quit()
+		sys.exit()
 
-	def reset(self):
-		self.camera.reset()
-		self.lvl.reset()
-		self.player.reset()
-
-
-	def _event_loop(self):
-		# ---------- User Events ----------
+	def _event_loop(self): # Event handling
+		# User events
+		mouse_pos = pygame.mouse.get_pos()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				self.close()
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					self.close()
-				if event.key == pygame.K_RETURN and self.player.dead:
-					self.reset()
-			self.player.handle_event(event)
+				#self.close()
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.KEYDOWN: 
+				if event.key == pygame.K_ESCAPE: # Pause
+					self.paused = not self.paused # Toggle value
+			elif event.type == pygame.MOUSEBUTTONDOWN: 
+				if self.player.dead: # If game over, process menu buttons to prevent false clicks
+					self.mainmenu_button.check_click(mouse_pos)
+					self.restart_button.check_click(mouse_pos)
+				if self.paused: # If paused, process pause menu buttons
+					self.resume_button.check_click(mouse_pos)
+					self.mainmenu_button_pause.check_click(mouse_pos)
+					self.restart_button_pause.check_click(mouse_pos)
 
+			if not self.paused: # If not paused, process user events
+				self.player.handle_event(event)
 
 	def _update_loop(self):
-		# ----------- Update -----------
+		# Update surfaces
 		self.player.update()
 		self.lvl.update()
 
 		if not self.player.dead:
-			self.camera.update(self.player.rect)
-			#calculate score and update UI txt
-			self.score=-self.camera.state.y//50
-			self.score_txt = config.SMALL_FONT.render(
-				str(self.score)+" m", 1, config.GRAY)
-	
+			self.camera.update(self.player.rect) # Update camera movement
+			# Calculate score and display it
+			self.score=-self.camera.state.y//50 # Update score
 
 	def _render_loop(self):
-		# ----------- Display -----------
-		self.window.fill(config.WHITE)
-		self.lvl.draw(self.window)
-		self.player.draw(self.window)
+		global window, clock
+		# Display objects
 
-		# User Interface
-		if self.player.dead:
-			self.window.blit(self.gameover_txt,self.gameover_rect)# gameover txt
-		self.window.blit(self.score_txt, self.score_pos)# score txt
+		# Calculate background offset based on camera position
+		offset_y = -self.camera.state.y % config.YWIN
+		# Display background
+		window.blit(self.background_image, (0, offset_y)) 
+		window.blit(self.background_image, (0, offset_y - config.YWIN))
 
-		pygame.display.update()# window update
-		self.clock.tick(config.FPS)# max loop/s
+		# Draw objects using Sprite.py
+		self.lvl.draw(window)
+		self.player.draw(window)
 
+		# Assign functions to buttons
+		# Buttons for game over
+		self.mainmenu_button.action = self.launch_main_menu
+		self.restart_button.action = self.reset
+		# Buttons for pause
+		self.resume_button.action = self.resume
+		self.mainmenu_button_pause.action = self.launch_main_menu
+		self.restart_button_pause.action = self.reset
+		
+		################## DRAWING OBJECTS BASED ON EVENTS ##################
+		if not self.player.dead and self.paused: # Pause
+			mouse_pos = pygame.mouse.get_pos() # Get current mouse position
+			window.blit(self.frame_image_pause, (pygame.math.Vector2(config.HALF_XWIN - 185, config.YWIN // 4 - 50))) # Draw frame for text
+			display_text('PAUSE', 65, config.WHITE, config.pixel_font, (pygame.math.Vector2(config.HALF_XWIN, config.YWIN // 4))) # Draw "PAUSE" text
+			
+			# Handle button hover reactions
+            # Resume button
+			if self.resume_button.rect.collidepoint(mouse_pos):  # If collision, switch to active image
+				if self.resume_button_sound: # Play button sound only once
+					config.check_button.play()
+					self.resume_button_sound = False
+				window.blit(pygame.image.load(config.resume_button_active).convert(), self.resume_button.rect)
+			else: # Change to default image
+				self.resume_button_sound = True
+				window.blit(self.resume_button.image.convert(), self.resume_button.rect) 
+
+			# Main menu
+			if self.mainmenu_button_pause.rect.collidepoint(mouse_pos): # If there is collision, change to active image
+				if self.mainmenu_button_sound: # Play button sound only once
+					config.check_button.play()
+					self.mainmenu_button_sound = False
+				window.blit(pygame.image.load(config.mainmenu_button_active).convert(), self.mainmenu_button_pause.rect)
+			else: # Change to default image
+				self.mainmenu_button_sound = True
+				window.blit(self.mainmenu_button_pause.image.convert(), self.mainmenu_button_pause.rect)
+
+			# Restart
+			if self.restart_button_pause.rect.collidepoint(mouse_pos): # If there is collision, change to active image 
+				if self.restart_button_sound: # Play button sound only once
+					config.check_button.play()
+					self.restart_button_sound = False
+				window.blit(pygame.image.load(config.restart_button_active).convert(), self.restart_button_pause.rect)
+			else: # Change to default image
+				self.restart_button_sound = True
+				window.blit(self.restart_button_pause.image.convert(), self.restart_button_pause.rect)
+
+		if self.player.dead: # Game over
+			# Zeroing the results of the frame if the animation is over
+			if self.frame_animation_end: 
+				self.frame_rect_y = self.frame_rect_initial_y
+				self.frame_animation_end = False
+			self.frame_reverse_animation_end = False 
+
+			if config.music_volume >= 0.0:
+				config.music_volume -= 0.001
+			pygame.mixer.music.set_volume(config.music_volume)
+
+			# Screen dimming
+			self.overlay_alpha = min(self.overlay_alpha + 5, 200)
+			if self.overlay_alpha >= 200:
+				self.max_darkness_reached = True
+
+				# Animation of moving buttons and images
+				# Game over
+				if self.gameover_rect_y < 15:
+					self.gameover_rect_y += 10
+				else:
+					self.game_over_animation_end = True
+				
+				# Main menu button
+				if self.mainmenu_button.rect.x < config.XWIN // 3 - self.mainmenu_button.rect[2] // 2:
+					self.mainmenu_button.rect.x += 10
+				else:
+					self.mainmenu_button_animation_end = True
+				
+				# Restart button
+				if self.restart_button.rect.x > config.XWIN // 1.5 - self.restart_button.rect[2] // 2:
+					self.restart_button.rect.x -= 13
+				else:
+					self.restart_button_animation_end = True
+
+		else:
+			# Zeroing values
+            # Decrease the alpha channel of the fade over time (this is where the fade rate is set)
+			if config.music_volume < 0.1:
+				config.music_volume += 0.01
+			pygame.mixer.music.set_volume(config.music_volume)
+			self.game_over_sound_played = False
+			self.overlay_alpha = max(self.overlay_alpha - 5, 0)
+			self.max_darkness_reached = False
+			self.game_over_animation_end = False
+			self.mainmenu_button_animation_end = False
+			self.restart_button_animation_end = False
+			self.score_txt_visible = True
+			self.gameover_rect_y = self.gameover_rect_initial_y
+			self.mainmenu_button.rect.x = self.mainmenu_button_rect_initial_x
+			self.restart_button.rect.x = self.restart_button_rect_initial_x
+		
+		# Creating a surface and applying shading to it
+		overlay_surface = pygame.Surface((config.XWIN, config.YWIN))
+		overlay_surface.fill(self.overlay_color)
+		overlay_surface.set_alpha(self.overlay_alpha)
+		if self.overlay_alpha != 0:
+			self.score_txt_visible = False
+			window.blit(overlay_surface, (0, 0))
+		
+		# Game process
+		if self.score_txt_visible or not self.frame_reverse_animation_end:
+			# High score counter animation
+			if not self.frame_animation_end and self.score_txt_visible:
+				if self.frame_rect_y < 10:
+					self.frame_rect_y += 2
+				else:
+					self.frame_animation_end = True
+			
+			# High score counter animation
+			elif not self.frame_reverse_animation_end:
+				if self.frame_rect_y >= -50:
+					self.frame_rect_y -= 10
+				else:
+					self.frame_reverse_animation_end = True
+
+			# Displaying
+			window.blit(self.frame_image_score, (config.XWIN // 6, self.frame_rect_y)) # Frame
+			display_text(str(self.score) + "m", 30, config.WHITE, config.pixel_font, (pygame.math.Vector2(config.XWIN // 3.1, self.frame_rect_y + 25))) # Score text
+
+		# Displaying game over objects
+		if self.max_darkness_reached: # if darkening ended
+			if not self.game_over_sound_played:
+				config.game_over.play()
+				self.game_over_sound_played = True
+
+			window.blit(self.game_over_image, (config.HALF_XWIN - self.gameover_rect.width // 2, self.gameover_rect_y)) # Displaying game over images
+			display_text(" SCORE:" + str(self.score) + "m", 40, config.WHITE, config.pixel_font, (config.HALF_XWIN, self.gameover_rect_y + config.YWIN // 1.5)) # Displaying score
+			mouse_pos = pygame.mouse.get_pos() # Getting mouse position
+				
+			# Processing the reaction of buttons on hovering the cursor
+			# Main menu
+			if self.mainmenu_button.rect.collidepoint(mouse_pos) and self.game_over_animation_end: # If there is collision, change to active image 
+				if self.mainmenu_button_sound: # Play button sound only once
+					config.check_button.play()
+					self.mainmenu_button_sound = False
+				window.blit(pygame.image.load(config.mainmenu_button_active).convert(), self.mainmenu_button.rect)
+			else: # Change to default image
+				self.mainmenu_button_sound = True
+				window.blit(self.mainmenu_button.image.convert(), self.mainmenu_button.rect)
+
+			# Restart
+			if self.restart_button.rect.collidepoint(mouse_pos) and self.game_over_animation_end: # If there is collision, change to active image 
+				if self.restart_button_sound: # Play button sound only once
+					config.check_button.play()
+					self.restart_button_sound = False
+				window.blit(pygame.image.load(config.restart_button_active).convert(), self.restart_button.rect)
+			else: # Change to default image
+				self.restart_button_sound = True
+				window.blit(self.restart_button.image.convert(), self.restart_button.rect)
+        
+		# Updating scene
+		pygame.display.update()
+		clock.tick(config.FPS)
 
 	def run(self):
-		# ============= MAIN GAME LOOP =============
-		while self.__alive:
-			self._event_loop()
-			self._update_loop()
-			self._render_loop()
-		pygame.quit()
+		# Main game loop
+		pygame.mixer.music.load(config.level_music)
+		pygame.mixer.music.set_volume(config.music_volume)
+		pygame.mixer.music.play(-1)
+		while True:
+			self._event_loop() # Event handling
+			self._render_loop() # Event render
+			if not self.paused:
+				self._update_loop() # Update surface
 
-
-
-
-if __name__ == "__main__":
-	# ============= PROGRAM STARTS HERE =============
+def run_game(): # Game launch function
 	game = Game()
 	game.run()
+
+if __name__ == "__main__":
+	# Program start
+	run_game()
+
+
+
 
--- settings.py
+++ settings.py
@@ -1,41 +1,122 @@
-# -*- coding: utf-8 -*-
-from pygame.font import SysFont
-from pygame import init
-init()
-# ==================================
+import pygame
+from button import Button
+pygame.init()
 
-#Window Settings
-XWIN, YWIN = 600,800 #                Resolution
-HALF_XWIN,HALF_YWIN = XWIN/2,YWIN/2 # Center
-DISPLAY = (XWIN,YWIN)
-FLAGS = 0 #                           Fullscreen, resizeable... 
-FPS = 60 #                            Render frame rate
-
-# Colors
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-GRAY = (100,100,100)
-LIGHT_GREEN = (131,252,107)
-ANDROID_GREEN = (164,198,57)
-FOREST_GREEN = (87,189,68)
-
-# Player
-PLAYER_SIZE = (25,35)
-PLAYER_COLOR = ANDROID_GREEN
-PLAYER_MAX_SPEED = 20
-PLAYER_JUMPFORCE = 20
-PLAYER_BONUS_JUMPFORCE = 70
-GRAVITY = .98
-
-# Platforms
-PLATFORM_COLOR = FOREST_GREEN
-PLATFORM_COLOR_LIGHT = LIGHT_GREEN
-PLATFORM_SIZE = (100,10)
-PLATFORM_DISTANCE_GAP = (50,210)
-MAX_PLATFORM_NUMBER = 10
-BONUS_SPAWN_CHANCE = 10
-BREAKABLE_PLATFORM_CHANCE = 12
+# Video settings
+XWIN, YWIN = 800, 800  # Resolution
+HALF_XWIN, HALF_YWIN = XWIN / 2, YWIN / 2  # Screen center
+DISPLAY = (XWIN, YWIN)  # Create screen instance
+FLAGS = pygame.DOUBLEBUF  # Screen options: 0 - none, pygame.FULLSCREEN - fullscreen, pygame.DOUBLEBUF - double buffering, pygame.SCALED - scaling
+FPS = 60
 
 # Fonts
-LARGE_FONT = SysFont("",128)
-SMALL_FONT = SysFont("arial",24)
+pixel_font = 'graphics\\font\\font.ttf'
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (100, 100, 100)
+LIGHT_GREEN = (131, 252, 107)
+ANDROID_GREEN = (164, 198, 57)
+FOREST_GREEN = (87, 189, 68)
+DARK_BLUE = (2, 3, 28)
+
+# Player
+PLAYER_SIZE = (90, 60)  # Size: width, height
+PLAYER_MAX_SPEED = 20  # Maximum speed
+PLAYER_JUMPFORCE = 20  # Maximum acceleration
+PLAYER_BONUS_JUMPFORCE = 70  # Maximum bonus acceleration
+GRAVITY = 0.92  # Gravity
+
+# Platform
+PLATFORM_SIZE = (110, 10)  # Platform size
+PLATFORM_DISTANCE_GAP = (50, 130)  # Distance between platforms
+MAX_PLATFORM_NUMBER = 12  # Maximum number of platforms for generation
+BONUS_WIDTH = 35  # Bonus width
+BONUS_HEIGHT = 35  # Bonus height
+BONUS_SPAWN_CHANCE = 6  # Bonus spawn chance
+BREAKABLE_PLATFORM_CHANCE = 6  # Breakable platform spawn chance
+
+# Music
+music_volume = 0.2
+level_music = 'audio\\level_music.wav'
+overworld_music = 'audio\\overworld_music.wav'
+
+# Sounds
+sound_volume = 1
+activate_button = 'audio\\effects\\activate_button.ogg'
+check_button = 'audio\\effects\\check_button.ogg'
+jump = 'audio\\effects\\jump.ogg'
+stomp = 'audio\\effects\\stomp.ogg'
+explosion = 'audio\\effects\\explosion.ogg'
+game_over = 'audio\\effects\\game_over.ogg'
+
+activate_button = pygame.mixer.Sound(activate_button)
+activate_button.set_volume(1)
+
+check_button = pygame.mixer.Sound(check_button)
+check_button.set_volume(1)
+
+jump = pygame.mixer.Sound(jump)
+jump.set_volume(0.1)
+
+stomp = pygame.mixer.Sound(stomp)
+stomp.set_volume(1)
+
+explosion = pygame.mixer.Sound(explosion)
+explosion.set_volume(1)
+
+game_over = pygame.mixer.Sound(game_over)
+game_over.set_volume(0.5)
+
+# Animation
+jumping = [  # Jumping animation
+    'graphics\\character\\jump\\1.png',
+]
+
+falling = [  # Falling animation
+    'graphics\\character\\fall\\2.png',
+    'graphics\\character\\fall\\3.png',
+    'graphics\\character\\fall\\4.png',
+    'graphics\\character\\fall\\5.png',
+]
+
+bonus_explosion = [  # Bonus explosion animation
+    'graphics\\bonus\\explosion\\1.png',
+    'graphics\\bonus\\explosion\\2.png',
+    'graphics\\bonus\\explosion\\3.png',
+    'graphics\\bonus\\explosion\\4.png',
+    'graphics\\bonus\\explosion\\5.png',
+    'graphics\\bonus\\explosion\\6.png',
+]
+
+# Images
+# For gameplay
+background_image = 'graphics\\background\\background.png'
+bonus_default_image = 'graphics\\bonus\\defaultsituation\\power_keg_with_m.png'
+platform_image = 'graphics\\platform\\normal\\platform.png'
+broken_platform_image = 'graphics\\platform\\broken\\broken-platform.png'
+
+# For the main menu
+main_menu_background = 'graphics\\main_menu\\background\\background.png'
+exit_button_non_active = 'graphics\\main_menu\\button\\non_active\\exit_btn.png'
+start_button_non_active = 'graphics\\main_menu\\button\\non_active\\start_btn.png'
+exit_button_active = 'graphics\\main_menu\\button\\active\\exit_btn.png'
+start_button_active = 'graphics\\main_menu\\button\\active\\start_btn.png'
+marwin_image = 'graphics\\main_menu\\marwin.png'
+
+# For the game over and pause menus
+game_over_image = 'graphics\\game_over_menu\\game_over.png'
+restart_button_non_active = 'graphics\\game_over_menu\\buttons\\non_active\\restart.png'
+mainmenu_button_non_active = 'graphics\\game_over_menu\\buttons\\non_active\\main_menu.png'
+resume_button_non_active = 'graphics\\pause\\buttons\\non_active\\resume.png'
+restart_button_active = 'graphics\\game_over_menu\\buttons\\active\\restart.png'
+mainmenu_button_active = 'graphics\\game_over_menu\\buttons\\active\\main_menu.png'
+resume_button_active = 'graphics\\pause\\buttons\\active\\resume.png'
+
+# UI
+frame_image_score = 'graphics\\UI\\frame.png'
+frame_image_pause = 'graphics\\pause\\frame.png'
+
+# Icon
+icon = 'graphics\\UI\\m.png'
